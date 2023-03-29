b05d0a2b33591b956e03d3d8ca309c3fa9680571
popatre <76402245+popatre@users.noreply.github.com>
2023-03-29 17:37:01 +0100

    add image and placeholder text to about

---
+++ src/components/Image.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Image() {
+    return (
+        <div>
+            <img
+                className="border rounded-full border-gray-400 m-10"
+                src="https://res.cloudinary.com/da5ahdojl/image/upload/v1680106308/mum_x4auas.webp"
+                alt="Dr Kim McGuire"
+            />
+        </div>
+    );
+}
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,27 @@
 import React from "react";
+import Image from "../components/Image";
+import Navbar from "../components/Navbar";
 
 export default function aboutMe() {
-    return <div>about-me</div>;
+    return (
+        <>
+            <Navbar />
+            <main className="mx-auto max-w-4xl w-9/10 flex items-center flex-col">
+                <Image />
+                <h1 className="text-4xl font-bold mb-10">Dr Kim McGuire</h1>
+                <p className="mx-10 my-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat corrupti vero eius dolorum nobis laudantium optio,
+                    ipsa consequuntur debitis vel cumque ad? Rem animi nobis
+                    rerum officia, iste obcaecati non!
+                </p>
+                <p className="mx-10 my-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Quaerat corrupti vero eius dolorum nobis laudantium optio,
+                    ipsa consequuntur debitis vel cumque ad? Rem animi nobis
+                    rerum officia, iste obcaecati non!
+                </p>
+            </main>
+        </>
+    );
 }
