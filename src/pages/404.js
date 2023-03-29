f6f84ce049b67f78437ded83d6684ee518a14df8
popatre <76402245+popatre@users.noreply.github.com>
2023-03-29 16:22:58 +0100

    initial page setup

---
+++ src/pages/404.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function NotFound() {
+    return <div>Sorry, that page doesnt exist</div>;
+}
---
+++ src/pages/about-me.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function aboutMe() {
+    return <div>about-me</div>;
+}
